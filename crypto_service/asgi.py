b91c1d0b36dd981bb2231c78e6d1d504968c3c1f
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 02:53:13 +0430

    channels asgi websocket configurations added to asgi.py

--- crypto_service/asgi.py
+++ crypto_service/asgi.py
@@ -9,8 +9,19 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from one_min_price.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crypto_service.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            ws_urlpatterns
+        ),
+    )
+})
