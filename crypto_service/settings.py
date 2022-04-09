09312aeda57c0fc33b66de94fea9af5b34aa0a64
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 03:51:06 +0430

    celery configurations added

--- crypto_service/settings.py
+++ crypto_service/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery Configuration Options
+CELERY_TIMEZONE = "Asia/Tehran"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
