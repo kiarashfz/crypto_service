37c61442956845ae50cfdab8a8b540347e66f385
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 03:51:45 +0430

    test celery task used for sending message to websocket

--- one_min_price/consumers.py
+++ one_min_price/consumers.py
@@ -4,11 +4,15 @@ from time import sleep
 
 from channels.generic.websocket import WebsocketConsumer
 
+from one_min_price.tasks import add
+
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
 
         for i in range(1000):
-            self.send(json.dumps({'message': randint(1, 1000)}))
+            func = add.delay(randint(1, 10), randint(1, 10))
+            res = func.get()
+            self.send(json.dumps({'message': res}))
             sleep(1)
