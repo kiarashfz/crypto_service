5aad5f10d99184a2aed7673c51904950de872817
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 07:17:50 +0430

    AsyncWebsocketConsumer used and the real celery task used on it

--- one_min_price/consumers.py
+++ one_min_price/consumers.py
@@ -1,18 +1,12 @@
 import json
-from random import randint
-from time import sleep
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-from one_min_price.tasks import add
+from one_min_price.tasks import up_down
 
 
-class WSConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-
-        for i in range(1000):
-            func = add.delay(randint(1, 10), randint(1, 10))
-            res = func.get()
-            self.send(json.dumps({'message': res}))
-            sleep(1)
+class WSConsumer(AsyncWebsocketConsumer):
+    async def receive(self, text_data=None, bytes_data=None):
+        text_data = json.loads(text_data)
+        await self.send(json.dumps({'message': up_down.delay(text_data['symbol'], float(text_data['price'])).get()}))
+        # return await super().receive(text_data, bytes_data)
