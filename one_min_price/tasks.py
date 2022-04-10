8bd7ba4ec99282766f02dcf0a035ee7ed1d59fd1
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 07:08:15 +0430

    up or down celery task get last binance candlestick & return with candle timestamp

--- one_min_price/tasks.py
+++ one_min_price/tasks.py
@@ -1,11 +1,15 @@
 from celery import shared_task
+from binance import Client
+from datetime import datetime
 
 
 @shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
+def up_down(symbol: str, price: float):
+    client = Client()
+    last_candle = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE)[-1]
+    candle_price = float(last_candle[4])
+    candle_timestamp = datetime.fromtimestamp(float(last_candle[0]) / 1000)
+    if candle_price > price:
+        return f'time:{candle_timestamp} --- Up'
+    else:
+        return f'time:{candle_timestamp} --- Down'
