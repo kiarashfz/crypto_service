42ad2afe155fa4a0663482446e2c950507359291
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 07:16:29 +0430

    celery settings completed

--- crypto_service/settings.py
+++ crypto_service/settings.py
@@ -130,3 +130,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_TIMEZONE = "Asia/Tehran"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_TRANSPORT = 'redis'
+CELERY_RESULT_BACKEND = 'mongodb://localhost:27017/'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+# from celery.schedules import crontab
+# CELERY_BEAT_SCHEDULE = {
+#     'every-minute': {
+#         'task': 'one_min_price.tasks.up_down',
+#         'schedule': crontab(minute='*/1'),
+#         'args': ('BTCUSDT', 999999),
+#     },
+# }
