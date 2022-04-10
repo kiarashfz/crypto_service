7796ffdd0fa32da8b1f71cbd583f1abc9aefa0ed
kiarashfz <kiawfz3673@gmail.com>
2022-04-10 07:47:39 +0430

    view object changed to FormView

--- one_min_price/views.py
+++ one_min_price/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+
+from one_min_price.forms import CryptoPriceChooserForm
 
 
-class Index(TemplateView):
+class Index(FormView):
     template_name = 'crypto-chooser.html'
+    form_class = CryptoPriceChooserForm
