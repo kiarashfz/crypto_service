41abd00f234c0968272c253697dc542354182b60
kiarashfz <kiawfz3673@gmail.com>
2022-04-11 19:00:52 +0430

    channel layer & group added to consumer

--- one_min_price/consumers.py
+++ one_min_price/consumers.py
@@ -6,7 +6,29 @@ from one_min_price.tasks import up_down
 
 
 class WSConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.channel_group_name = self.scope['url_route']['kwargs']['channel_name']
+
+        await self.channel_layer.group_add(
+            self.channel_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
     async def receive(self, text_data=None, bytes_data=None):
         text_data = json.loads(text_data)
-        await self.send(json.dumps({'message': up_down.delay(text_data['symbol'], float(text_data['price'])).get()}))
-        # return await super().receive(text_data, bytes_data)
+        await self.channel_layer.group_send(
+            self.channel_group_name,
+            {
+                'type': 'channel_message',
+                'message': up_down.delay(text_data['symbol'], float(text_data['price'])).get()
+            }
+        )
+
+    async def channel_message(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
