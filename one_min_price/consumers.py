78e3f4686e7f651ba756826b895c6eadd907d1c1
kiarashfz <kiawfz3673@gmail.com>
2022-04-12 00:09:32 +0430

    schedule & periodictask created by receiving args from websocket & disconnecting handled

--- one_min_price/consumers.py
+++ one_min_price/consumers.py
@@ -1,8 +1,8 @@
 import json
 
+from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
-
-from one_min_price.tasks import up_down
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
 
 
 class WSConsumer(AsyncWebsocketConsumer):
@@ -18,12 +18,20 @@ class WSConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data=None, bytes_data=None):
         text_data = json.loads(text_data)
-        await self.channel_layer.group_send(
-            self.channel_group_name,
-            {
-                'type': 'channel_message',
-                'message': up_down.delay(text_data['symbol'], float(text_data['price'])).get()
-            }
+        schedule, created = await sync_to_async(IntervalSchedule.objects.get_or_create)(
+            every=1,
+            period=IntervalSchedule.MINUTES,
+        )
+
+        self.task, created = await sync_to_async(PeriodicTask.objects.get_or_create)(
+            interval=schedule,
+            name=f'{self.channel_group_name}',
+            task='one_min_price.tasks.up_down',
+            kwargs=json.dumps({
+                'symbol': text_data['symbol'],
+                'price': float(text_data['price']),
+                'channel_group_name': self.channel_group_name,
+            }),
         )
 
     async def channel_message(self, event):
@@ -32,3 +40,8 @@ class WSConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message
         }))
+
+    async def disconnect(self, code):
+        self.task.enabled = False
+        await sync_to_async(self.task.save)()
+        return await super().disconnect(code)
