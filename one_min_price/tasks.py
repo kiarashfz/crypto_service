43a902710d5a06b4ce1ac7453e1302f94100a37a
kiarashfz <kiawfz3673@gmail.com>
2022-04-12 00:01:55 +0430

    sending message to websocket channels added to task

--- one_min_price/tasks.py
+++ one_min_price/tasks.py
@@ -1,15 +1,30 @@
+from asgiref.sync import async_to_sync
 from celery import shared_task
 from binance import Client
 from datetime import datetime
 
+from channels.layers import get_channel_layer
+
+channel_layer = get_channel_layer()
+
 
 @shared_task
-def up_down(symbol: str, price: float):
+def up_down(**kwargs):
     client = Client()
-    last_candle = client.get_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_1MINUTE, limit=1)[0]
+    last_candle = client.get_klines(symbol=kwargs['symbol'], interval=Client.KLINE_INTERVAL_1MINUTE, limit=1)[0]
     candle_price = float(last_candle[4])
     candle_timestamp = datetime.fromtimestamp(float(last_candle[0]) / 1000)
-    if candle_price > price:
-        return {'timestamp': candle_timestamp, 'input_price': price, 'close_price': candle_price, 'status': 'Up'}
+    if candle_price > kwargs['price']:
+        async_to_sync(channel_layer.group_send)(kwargs['channel_group_name'], {'type': 'channel_message',
+                                                                               'message': {
+                                                                                   'timestamp': str(candle_timestamp),
+                                                                                   'input_price': kwargs['price'],
+                                                                                   'close_price': candle_price,
+                                                                                   'status': 'Up'}})
     else:
-        return {'timestamp': candle_timestamp, 'input_price': price, 'close_price': candle_price, 'status': 'Down'}
+        async_to_sync(channel_layer.group_send)(kwargs['channel_group_name'], {'type': 'channel_message',
+                                                                               'message': {
+                                                                                   'timestamp': str(candle_timestamp),
+                                                                                   'input_price': kwargs['price'],
+                                                                                   'close_price': candle_price,
+                                                                                   'status': 'Down'}})
