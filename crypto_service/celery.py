9539677333220d9ae026b9fb20c9cd776a7d2ef2
kiarashfz <kiawfz3673@gmail.com>
2022-04-11 18:45:10 +0430

    test task removed

--- crypto_service/celery.py
+++ crypto_service/celery.py
@@ -24,8 +24,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
