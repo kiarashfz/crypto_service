1350669eec0d8304fdc6113a4761efdc4641065d
kiarashfz <kiawfz3673@gmail.com>
2022-04-11 18:55:58 +0430

    paired crypto choices load from redis api cache

--- one_min_price/forms.py
+++ one_min_price/forms.py
@@ -1,13 +1,13 @@
-from binance import Client
+from crypto_service.service import paired_cryptos_cache_checker
+from django_redis import get_redis_connection
 from django import forms
+
+cache = get_redis_connection('default')
+paired_cryptos_cache_checker()
 
 
 class CryptoPriceChooserForm(forms.Form):
-    PAIRED_CRYPTOS_CHOICES = []
-    client = Client()
-    exchange_info = client.get_exchange_info()
-    for s in exchange_info['symbols']:
-        PAIRED_CRYPTOS_CHOICES.append((s['symbol'], s['symbol']))
-
-    symbol = forms.ChoiceField(choices=PAIRED_CRYPTOS_CHOICES)
+    symbol = forms.ChoiceField(choices=[tuple(str(pair.decode("utf-8")).split(",")) for pair in
+                                        cache.lrange('CS_PAIRED_CRYPTOS_CHOICES', 0, -1)])
     price = forms.IntegerField(min_value=0)
+    channel_name = forms.CharField(max_length=100)
